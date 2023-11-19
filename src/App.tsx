f10d45c73c58e4a511712453173c361b4d861c02
11 11 <arf1385@gmail.com>
2023-11-19 19:44:07 +0330

    Reverts 987d181b610c48bb439bb811bd2c24a1093a35ca
    As Tailwind CSS doesn't support runtime css generation, the recommended
    (as the developers themselves suggest) is using inline/element style to
    change margin dynamically at runtime in css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,43 @@
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function ClickWrapper() {
   const [count, setCount] = useState(0);
   return (
-    <div className='text-center'>
+    <div className="text-center">
       <h1>Welcome to my App!</h1>
       <p>Click the image to increase the count!</p>
       <img
-        onClick={() => { setCount(count + 1) }}
+        onClick={() => {
+          setCount(count + 1);
+        }}
         src="https://reactjs.org/logo-og.png"
         alt="React"
-        className=
-          {`mx-[${count}] \
-          origin-center \
-          translate-x-1/2 \
-          transition-all \
-          border-[5px] \
-          border-violet-600 \
-          hover:rounded-ss-[50%] \
-          hover:rounded-ee-[50%] \
-          bg-red-300 w-1/2`} />
-      <MyButton counts={count} onClick={() => { setCount(0) }} />
+        className="origin-center translate-x-1/2 transition-all border-[5px] border-violet-600 hover:rounded-ss-[50%] hover:rounded-ee-[50%] bg-red-300 w-1/2"
+        style={{ margin: `${count}px 0` }}
+      />
+      <MyButton
+        counts={count}
+        onClick={() => {
+          setCount(0);
+        }}
+      />
     </div>
   );
 }
 
-function MyButton(props: {counts: number, onClick: () => void}) {
+function MyButton(props: { counts: number; onClick: () => void }) {
   return (
-    <button onClick={props.onClick}title='Click to reset'>I&#39;m a button!<br /> clicked: {props.counts} <br/>Click me to reset!</button>
+    <button onClick={props.onClick} title="Click to reset">
+      I&#39;m a button!
+      <br /> clicked: {props.counts} <br />
+      Click me to reset!
+    </button>
   );
 }
 
 function App() {
-  return (
-    <ClickWrapper />
-  );
+  return <ClickWrapper />;
 }
 
-export default App
+export default App;
